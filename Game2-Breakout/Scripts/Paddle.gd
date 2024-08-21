81992cf693b7cb1ea161d1fab4e4047bc79254c3
Lucy Daniels <lucy.daniels.software@gmail.com>
2024-08-21 20:51:38 +0100

    Event Bus Part 1

--- Game2-Breakout/Scripts/Paddle.gd
+++ Game2-Breakout/Scripts/Paddle.gd
@@ -7,9 +7,19 @@ class_name Paddle extends CharacterBody2D
 @onready var ballPos : Node2D = $BallLockPosition
 @onready var shape: CollisionPolygon2D = $CollisionShape2D as CollisionPolygon2D
 
+var _targetPosition:= Vector2.ZERO
+
+func shoot(targetPosition : Vector2) -> void:
+	if _ball.locked:
+		var shootDir := Vector2.from_angle(get_angle_to(targetPosition))
+		_ball.start(shootDir)
+	
+
+func setTargetPosition(localPos: Vector2) -> void:
+	_targetPosition = localPos
+
 func _physics_process(delta: float):
-	var target_position = get_global_mouse_position()
-	var moveResult = global_position.x - move_toward(global_position.x, target_position.x, paddle_speed * delta)
+	var moveResult = position.x - move_toward(position.x, _targetPosition.x, paddle_speed * delta)
 
 	var moveVector := Vector2(-moveResult,0)
 	move_and_collide(moveVector, false, 1.0)
