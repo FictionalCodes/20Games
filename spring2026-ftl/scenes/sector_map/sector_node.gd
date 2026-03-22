da7cc45bfb62c57ac6dc9177bb1cc0ba466d1b79
Onyx-Azzy <onyxazzy@gmail.com>
2026-03-22 00:37:31 +0000

    Sector Map Tooltip
    Added a sector map tooltip to display on mouse over.
    Added font to event_ui_theme
    Changed _set_node_colour in sector_node.gd to _get_sector_type_colour for reusability

--- spring2026-ftl/scenes/sector_map/sector_node.gd
+++ spring2026-ftl/scenes/sector_map/sector_node.gd
@@ -15,27 +15,36 @@ var sector_type : String
 
 # @onready variables
 @onready var texture: TextureRect = %Texture
+@onready var tooltip: PanelContainer = %Tooltip
 
 
 func _ready() -> void:
-	#mouse_entered.connect(_on_mouse_entered)
-	#mouse_exited.connect(_on_mouse_exited)
-	_set_node_colour(sector_type)
+	texture.mouse_entered.connect(_on_mouse_entered)
+	texture.mouse_exited.connect(_on_mouse_exited)
+	texture.modulate = _get_sector_type_colour()
+	_set_label_text()
+	tooltip.hide()
 
 
-func _set_node_colour(sector_type : String) -> void:
+func _get_sector_type_colour() -> Color:
 	var colour : Color
 	match sector_type:
 		'Civilian': colour = Color.GREEN
 		'Hostile': colour = Color.RED
 		'Nebula': colour = Color.PURPLE
-	texture.modulate = colour
+	return colour
 
+
+func _set_label_text() -> void:
+	%SectorNameLabel.text = 'Unexplored Sector'
+	%SectorTypeLabel.text = sector_type
+	%SectorTypeLabel.modulate = _get_sector_type_colour()
+	%DescriptionLabel.text = 'A region of unexplored space'
 
 func _on_mouse_entered() -> void:
-	print("node entered")
+	tooltip.show()
 
 
 func _on_mouse_exited() -> void:
-	print("node exited")
+	tooltip.hide()
 	
