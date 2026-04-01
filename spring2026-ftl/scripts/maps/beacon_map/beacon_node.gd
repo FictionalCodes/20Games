2d706fffb46bcd816f15e26ee804a6bd97aab337
Onyx-Azzy <onyxazzy@gmail.com>
2026-04-01 09:48:17 +0100

    Animated Beacon Lines
    Added line animation for the beacon connections

--- spring2026-ftl/scripts/maps/beacon_map/beacon_node.gd
+++ spring2026-ftl/scripts/maps/beacon_map/beacon_node.gd
@@ -2,19 +2,23 @@ class_name BeaconNode extends Node2D
 
 ## The beacon node is a component of the beacon map. 
 
+# Constants
+const DOTTED_LINE = preload("uid://djyat7v1b4kg7")
+const ANIMATED_LINE = preload("uid://boxbwcs7tsgt8")
+
 # @export variables
 @export var node_texture: TextureRect
 @export var tooltip: PanelContainer
 @export var area: Area2D
 @export var current_beacon_indicator: Polygon2D
 @export var exit_beacon_indicator: Label
+@export var click_zone: Area2D
 
 # regular variables
 var neighbours : Array[Area2D]
 var lines : Array[Line2D]
 var is_current_beacon : bool
 var is_exit_beacon: bool
-
 
 func _ready() -> void:
 	_connect_signals()
@@ -23,9 +27,9 @@ func _ready() -> void:
 
 
 func _connect_signals() -> void:
-	node_texture.mouse_entered.connect(_on_mouse_entered)
-	node_texture.mouse_exited.connect(_on_mouse_exited)
-	
+	click_zone.mouse_entered.connect(_on_mouse_entered)
+	click_zone.mouse_exited.connect(_on_mouse_exited)
+
 
 ## Connect all neighbouring nodes
 func connect_neighbours() -> void:
@@ -33,12 +37,20 @@ func connect_neighbours() -> void:
 	for neighbour in neighbours:
 		var line = Line2D.new()
 		add_child(line)
+		lines.append(line)
+		# Line Positioning
 		line.position = Vector2.ZERO
-		line.width = 2.0	
-		line.default_color = Color.YELLOW
 		line.add_point(Vector2.ZERO)
 		line.add_point(neighbour.global_position - global_position)
-		lines.append(line)
+		# Line Visuals
+		line.width = 5.0
+		#line.modulate = Color.YELLOW # TODO not working with animated line shader
+		line.texture = DOTTED_LINE
+		line.texture_mode = Line2D.LINE_TEXTURE_TILE
+		line.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED
+		line.material = ANIMATED_LINE
+
+		
 
 
 func _show_connections() -> void:
